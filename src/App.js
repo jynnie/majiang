f1ec345433db220905ae18454d60901721fb5c2a
jynnie <jynnie@mit.edu>
2020-08-16 21:40:07 -0700

    Firebase auth setup and basic infra for firebase db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+
+//- Component & Page Imports
+import Auth from "./components/Auth";
 import "./App.css";
 
-function App() {
-  return <div>Yes, boss.</div>;
-}
-export default App;
+//- Firebase Imports
+import withFirebaseAuth from "react-with-firebase-auth";
+import firebase from "firebase";
+import firebaseConfig from "./firebaseConfig";
+import "firebase/auth";
+
+//- Firebase Setup
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export const FirebaseContext = React.createContext(null);
+
+//- App Setup
+const App = (props) => {
+  const { user, signOut, signInWithGoogle } = props;
+
+  return (
+    <FirebaseContext.Provider value={{ user }}>
+      <Auth signOut={signOut} signInWithGoogle={signInWithGoogle} />
+    </FirebaseContext.Provider>
+  );
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/components/Auth.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import { FirebaseContext } from "../App";
+
+export const Auth = (props) => {
+  const { user, db } = useContext(FirebaseContext);
+  const { signOut, signInWithGoogle } = props;
+
+  const handleSignIn = (res) => {
+    // const usersRef = db.collection("users");
+    // const id = res.user.uid; // Returned from signIn() promise
+    // usersRef
+    //   .doc(id)
+    //   .get()
+    //   .then((doc) => {
+    //     if (!doc.exists) {
+    //       // If we have no user of this uid saved
+    //       usersRef.doc(id).set({
+    //         // Create a new user document
+    //         uid: id,
+    //         displayName: res.user.displayName,
+    //         provider: res.credential.providerId,
+    //       });
+    //     }
+    //   });
+  };
+
+  const handleSignInWithGoogle = () => {
+    if (signInWithGoogle) signInWithGoogle().then((res) => handleSignIn(res));
+  };
+
+  if (user)
+    console.debug(
+      `🥳 ${user.displayName} has signed in from ${user.providerData[0].providerId} 🥳`,
+    );
+
+  return (
+    <div>
+      {user && <span>Hello, {user.displayName}</span>}
+      {user ? (
+        <button onClick={signOut}>Sign out</button>
+      ) : (
+        <button onClick={handleSignInWithGoogle}>Sign in with Google</button>
+      )}
+    </div>
+  );
+};
+
+export default Auth;
