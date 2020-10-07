7b5f43a1733022d1bb3681f9246a682e4ac5f534
jynnie <jynnie@mit.edu>
2020-10-06 21:53:09 -0700

    🐛 Action: Chi button wasn't showing

--- src/components/Action/Action.jsx
+++ src/components/Action/Action.jsx
@@ -13,7 +13,7 @@ import {
 
 const Action = ({ name, onClick }) => {
   if (name === "Draw") return <DrawAction onClick={onClick} />;
-  if (name === "Chi") return <ChiAction onClick={onClick} />;
+  if (name.includes("Chi")) return <ChiAction onClick={onClick} />;
   if (name === "Peng") return <PengAction onClick={onClick} />;
   if (name === "Gang") return <GangAction onClick={onClick} />;
   if (name === "Hu") return <HuAction onClick={onClick} />;
